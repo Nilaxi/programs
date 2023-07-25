3ccb9d807223159b24d0e72d5b2007979d9fc563
Nilaxi <nilaximakwana@gmail.com>
2023-07-25 23:02:04 +0530

    Box js

---
+++ loop.js
@@ -0,0 +1,28 @@
+var n = 5;
+var data = "";
+var test = "";
+
+// for(var i = 0; i < n; i++){
+//     for(var j = 0; j < n; j++){
+//         data += "*";
+//     }
+//     data += "\n";
+// }
+// console.log(data);
+
+// test = "test";
+// test += "jay mataji";
+// test += "Nikhil";
+// test += "nilaxi";
+// console.log(test);
+
+// for(var i = 1; i <= n; i++){
+//     for(var j = 0; j < n - i; j++){
+//         data += " ";
+//     }
+//     for(var k = 0; k < i; k++){
+//         data += "*";
+//     }
+//     data += "\n";
+// }
+// console.log(data);
